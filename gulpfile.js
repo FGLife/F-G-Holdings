81f427a259c0ea8210d7ee023fe42a0611ff8a3d
KatyClark <katy@katyclark.com>
2017-12-19 10:40:25 -0500

    Update gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf200
+{\fonttbl\f0\fnil\fcharset0 Consolas;}
+{\colortbl;\red255\green255\blue255;\red203\green35\blue57;\red244\green246\blue249;\red27\green31\blue34;
+\red7\green68\blue184;\red6\green33\blue79;\red91\green40\blue180;\red87\green96\blue106;}
+{\*\expandedcolortbl;;\cssrgb\c84314\c22745\c28627;\cssrgb\c96471\c97255\c98039;\cssrgb\c14118\c16078\c18039;
+\cssrgb\c0\c36078\c77255;\cssrgb\c1176\c18431\c38431;\cssrgb\c43529\c25882\c75686;\cssrgb\c41569\c45098\c49020;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\sl380\partightenfactor0
+
+\f0\fs27\fsmilli13600 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 var\cf4 \strokec4  gulp \cf2 \strokec2 =\cf4 \strokec4  \cf5 \strokec5 require\cf4 \strokec4 (\cf6 \strokec6 'gulp'\cf4 \strokec4 );\
+\
+gulp.\cf7 \strokec7 task\cf4 \strokec4 (\cf6 \strokec6 'default'\cf4 \strokec4 , \cf2 \strokec2 function\cf4 \strokec4 () \{\
+  \cf8 \strokec8 // place code for your default task here\cf4 \strokec4 \
+\});\
+}
